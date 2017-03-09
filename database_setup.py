a5ef3cdb28432baaf4117cab1312eddc2ec6ea5c
Keith <krozu@comcast.net>
2017-03-08 16:07:46 -0800

    Add files via upload

---
+++ database_setup.py
@@ -0,0 +1,61 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+class Genre(Base):
+    __tablename__ = "genre"
+
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def list(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+class Books(Base):
+    __tablename__ = "books"
+
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    author = Column(String(80), nullable = False)
+    description = Column(String(500))
+    price = Column(String(8))
+    rating = Column(Integer)
+    genre_id = Column(Integer, ForeignKey("genre.id"))
+    genre = relationship(Genre)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'author': self.author,
+            'id': self.id,
+            'price': self.price,
+            'rating': self.rating,
+        }
+
+
+
+engine = create_engine('sqlite:///booksellers.db')
+Base.metadata.create_all(engine)
